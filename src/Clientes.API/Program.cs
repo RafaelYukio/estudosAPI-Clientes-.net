5d1323c6ae6563462457b6dc644983078bfb9030
RafaelYukio <rafaelyut@hotmail.com>
2023-08-20 18:19:15 -0300

    feat: adicionado docker-compose para execução da API

--- src/Clientes.API/Program.cs
+++ src/Clientes.API/Program.cs
@@ -1,11 +1,25 @@
 using Clientes.Infra.IoC;
 using System;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbInjections(builder.Configuration);
+//builder.Services.AddDbInjections(builder.Configuration);
+// Variáveis definidas no docker para container do SQL
+// Tutorial de configuração do Docker:
+// https://www.youtube.com/watch?v=hpLvXNASyTI&list=FLawtE1QiQ3kttrk_KBFfGxw&index=1&pp=gAQB
+// https://www.twilio.com/blog/containerize-your-aspdotnet-core-application-and-sql-server-with-docker
+
+// Para arrumar o erro de conexão na imagem do SQL (resposta de sayago69):
+// https://github.com/dotnet/SqlClient/issues/633
+
+string dbServer = Environment.GetEnvironmentVariable("DB_SERVER");
+string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+builder.Services.AddDockerDbInjections(dbServer, dbName, dbPassword);
+
 builder.Services.AddClienteInjections();
 
 builder.Services.AddControllers();
---
+++ src/Clientes.Infra.IoC/DbDockerInjection.cs
@@ -0,0 +1,20 @@
+﻿using Clientes.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+namespace Clientes.Infra.IoC
+{
+    public static class DbDockerInjection
+    {
+        public static void AddDockerDbInjections(this IServiceCollection services,
+                                                 string dbServer,
+                                                 string dbName,
+                                                 string dbPassword)
+        {
+            string connectionString = $"Server={dbServer};Database={dbName};User Id=SA;Password={dbPassword};Trust Server Certificate=true;Encrypt=False";
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+        }
+    }
+}
